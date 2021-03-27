0d3290a36693b223bf04dbaf366773c5776214bf
Plisko Oleh <plisko2013@gmail.com>
2021-03-27 18:12:53 +0200

    Remake assertEquals

--- src/test/java/SecondAutoTest.java
+++ src/test/java/SecondAutoTest.java
@@ -2,18 +2,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class SecondAutoTest {
     WebDriver driver;
+    By searchResults = By.cssSelector(".catalog-grid .goods-tile__title");
+    String expectedResult = "Монитор";
 
     @BeforeClass
     public void setUP() {
         System.setProperty("webdriver.chrome.driver", "src/Driver/chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://rozetka.com.ua/");
     }
@@ -27,7 +34,9 @@ public class SecondAutoTest {
         WebElement searchButton = driver.findElement(By.cssSelector(".button.search-form__submit"));
         searchButton.click();
 
-        Assert.assertEquals("Монитор", "Монитор");
+        List<WebElement> results = driver.findElements(searchResults);
+        String firstResultTitle = results.get(0).getText();
+        Assert.assertEquals(firstResultTitle, "Mонитор 27\" Samsung Odyssey G5 LC27G54T Black (LC27G54TQWIXCI) + DisplayPort и HDMI кабель");
     }
 
     @AfterMethod
